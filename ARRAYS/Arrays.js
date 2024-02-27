2c91dfc5503a2573f402d778c49d72f076559d24
Odan Infotech <htank8866@gmail.com>
2024-02-27 18:40:48 +0530

    update new method

---
+++ ARRAYS/Arrays.js
@@ -0,0 +1,36 @@
+// // // // let heros =["Batman ","Thor","Spiderman","Hulk","Ironman","Antman"]
+// // // // //console.log(heros);
+// // // // for(let i=0;i<heros.length;i++)
+// // // // {
+// // // //     console.log(heros[i]);
+// // // // }
+// // // //AVERAGE OF MARKS WITH ARRAYS AND FOR LOOP
+// // // let marks=[85,97,44,37,76,60]
+// // // let sum = 0;
+// // // for(let val of marks)
+// // // {
+// // //     sum += val;
+// // // }
+// // // let avg = sum / marks.length;
+// // // console.log(`avg marks of class = ${avg}`);
+// // let item=[250,645,300,900,50]
+// // let i =0;
+// // for(let val of item)
+// // {
+// //     console.log(`value at index= ${i}=${val}`);
+// //     let offer = val / 10;
+// //     item[i]= item[i]-offer;
+// //     console.log(`value after offer = ${item[i]}`);
+// //     i++;
+
+// // }
+// //ARRAY METHODS
+// let fooditems =["potato","burger","cherry","banana","apple"]
+// //fooditems.push("frankey","ladyfingrs");
+// console.log(fooditems);
+// let deletedval=fooditems.pop();
+// console.log(deletedval);
+// //ARRAY CONVERT TO TOSTRINGS
+// console.log(fooditems.toString());
+//CONCAT ARRAY TWO ARRAY JOIN IN DIFFRENT NEW ARRAY
+        
--- STRING/string.js
+++ STRING/string.js
@@ -1,23 +1,38 @@
-// // // // STRING
-// // // let str="javaScript";
-// // // console.log(str[3]);
-// // // //Template 
-// // // let obj ={
-// // //     Item:"Bottle",
-// // //     price :"100",
-// // // };
+// // // // // // // STRING
+// // // // // // let str="javaScript";
+// // // // // // console.log(str[3]);
+// // // // // // //Template 
+// // // // // // let obj ={
+// // // // // //     Item:"Bottle",
+// // // // // //     price :"100",
+// // // // // // };
 
-// // // let output =`The cost of ${obj.Item} is ${obj.price}`;
-// // // console.log(output);
+// // // // // // let output =`The cost of ${obj.Item} is ${obj.price}`;
+// // // // // // console.log(output);
 
-// // // // \n CAN USE NEXT LINE and \t is calle TABSPACE
-// // let str ="This is \n \tjavaScript";
-// // //console.log("This is \n \tjavaScript");
-// // console.log(str.length);
-// let str ="Marvel";
-// let newStr = str.toUpperCase();
-// console.log(str);
-// console.log(newStr);
-// //trim SPACE FIRST AND LAST
-let str ="                  All the Best        ";
-console.log(str.trim());
+// // // // // // // \n CAN USE NEXT LINE and \t is calle TABSPACE
+// // // // // let str ="This is \n \tjavaScript";
+// // // // // //console.log("This is \n \tjavaScript");
+// // // // // console.log(str.length);
+// // // // let str ="Marvel";
+// // // // let newStr = str.toUpperCase();
+// // // // console.log(str);
+// // // // console.log(newStr);
+// // // // //trim SPACE FIRST AND LAST
+// // // let str ="                  All the Best        ";
+// // // console.log(str.trim());
+// // //BASIC PROGRAM USERNAME WITH LENGTH
+// // let fullName =prompt("Please Enter your name without space :");
+// // let userName ="@"+fullName+fullName.length;
+// // console.log(userName);
+// //SRING METHOD
+// let str= "0123456";
+// console.log(str.slice(1,4));
+// CONCAT
+let str1 = "SITA";
+let str2 = "RAM";
+ let res =str1.concat(str2);
+ console.log(res);
+ // OTHERWISE str.replace(search val,newval);
+ //str.charAt()
+ console.log(str1.charAt(2));
