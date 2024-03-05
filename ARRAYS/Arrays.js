c54c899c3971218947a72dc427952bf046eadcf1
Odan Infotech <htank8866@gmail.com>
2024-03-05 18:35:18 +0530

    include new folder

--- ARRAYS/Arrays.js
+++ ARRAYS/Arrays.js
@@ -1,36 +1,63 @@
-// // // // let heros =["Batman ","Thor","Spiderman","Hulk","Ironman","Antman"]
-// // // // //console.log(heros);
-// // // // for(let i=0;i<heros.length;i++)
+// // // // // let heros =["Batman ","Thor","Spiderman","Hulk","Ironman","Antman"]
+// // // // // //console.log(heros);
+// // // // // for(let i=0;i<heros.length;i++)
+// // // // // {
+// // // // //     console.log(heros[i]);
+// // // // // }
+// // // // //AVERAGE OF MARKS WITH ARRAYS AND FOR LOOP
+// // // // let marks=[85,97,44,37,76,60]
+// // // // let sum = 0;
+// // // // for(let val of marks)
 // // // // {
-// // // //     console.log(heros[i]);
+// // // //     sum += val;
 // // // // }
-// // // //AVERAGE OF MARKS WITH ARRAYS AND FOR LOOP
-// // // let marks=[85,97,44,37,76,60]
-// // // let sum = 0;
-// // // for(let val of marks)
+// // // // let avg = sum / marks.length;
+// // // // console.log(`avg marks of class = ${avg}`);
+// // // let item=[250,645,300,900,50]
+// // // let i =0;
+// // // for(let val of item)
 // // // {
-// // //     sum += val;
-// // // }
-// // // let avg = sum / marks.length;
-// // // console.log(`avg marks of class = ${avg}`);
-// // let item=[250,645,300,900,50]
-// // let i =0;
-// // for(let val of item)
-// // {
-// //     console.log(`value at index= ${i}=${val}`);
-// //     let offer = val / 10;
-// //     item[i]= item[i]-offer;
-// //     console.log(`value after offer = ${item[i]}`);
-// //     i++;
+// // //     console.log(`value at index= ${i}=${val}`);
+// // //     let offer = val / 10;
+// // //     item[i]= item[i]-offer;
+// // //     console.log(`value after offer = ${item[i]}`);
+// // //     i++;
 
-// // }
-// //ARRAY METHODS
-// let fooditems =["potato","burger","cherry","banana","apple"]
-// //fooditems.push("frankey","ladyfingrs");
-// console.log(fooditems);
-// let deletedval=fooditems.pop();
-// console.log(deletedval);
-// //ARRAY CONVERT TO TOSTRINGS
-// console.log(fooditems.toString());
-//CONCAT ARRAY TWO ARRAY JOIN IN DIFFRENT NEW ARRAY
-        
+// // // }
+// // //ARRAY METHODS
+// // let fooditems =["potato","burger","cherry","banana","apple"]
+// // //fooditems.push("frankey","ladyfingrs");
+// // console.log(fooditems);
+// // let deletedval=fooditems.pop();
+// // console.log(deletedval);
+// // //ARRAY CONVERT TO TOSTRINGS
+// // console.log(fooditems.toString());
+// //CONCAT ARRAY TWO ARRAY JOIN IN DIFFRENT NEW ARRAY
+//  //let marvel_heros=["batman","thor","spiderman","ironman"];
+// // let dc_heros=["superman","antman"];
+// // let heros = marvel_heros.concat(dc_heros);
+// // console.log(heros);
+// //marvel_heros.unshift("krish"); UNSHIFT AND SHIFT ARRAY USED TO ADD ND DELETED ARRAY
+// //SLICE() METHOD
+// //let heros=["batman","thor","spiderman","ironman","antman","superman"];
+// //console.log(heros);
+// //console.log(heros.slice(1, 4));
+// //SPLICE() ARE USED TO ORIGINAL ARRAY CHANGED ADD REPLACE REMOVE
+// let arr = [1,2,3,4,5,6];
+// //arr.splice(2,2,505,606);
+// //add element
+// //arr.splice(1,0,111);
+// //delete element
+// arr.splice(4,1);
+// //replace arry
+// arr.splice(4,1,12);
+
+// console.log(arr);
+
+//PRACTICE FOR ARRAY
+let companies =["Bloomberg","Microsoft","Uber","Google","Igm","Netflix"];
+console.log(companies);
+companies.shift();
+companies.splice(3,1,"Ola");
+companies.push("Amazone");
+console.log(companies);
---
+++ FUNCTION/Function.js
@@ -0,0 +1,88 @@
+// // // // //     function myFunction(){
+// // // // //         console.log("HEllo");
+// // // // //         console.log("Welcome to Js");
+// // // // //     }
+// // // // // myFunction();
+// // // // //sum of two numbers with function
+// // // // function sum(a,b){
+// // // //     s = a+b;
+// // // //     console.log("before return");
+// // // //     return s;
+// // // // }
+// // // // let val =sum(5,10);
+// // // // console.log(val);
+// // // //ARROW FUNCTION
+// // // function sum(a,b){
+// // //     return a + b;
+// // // }
+// // // const arrowsum= (a,b) =>{
+// // //     console.log(a+b);
+
+// // // };
+// // // function mul(a,b){
+// // //     return a*b;
+
+// // // }
+// // // const arrowmul= (a,b) =>{
+// // //     console.log(a*b);
+
+// // // };
+// // //COUNT VOWELS
+// // function countVowels(str){
+// //     let count = 0;
+// //     for(const char of str){
+// //         if(char === "a" || char === "e" || char ==="i" || char ==="i" || char ==="o" || char ==="u")
+// //         {count++;
+// //         }
+// //     }
+// //         return count;
+
+// // }
+// // //SAME PROGRAM WITH ARROW FUNCTION
+// // const countVow = (str) => {
+// //     let count = 0;
+// //     for(const char of str){
+// //         if(char === "a" || char === "e" || char ==="i" || char ==="i" || char ==="o" || char ==="u")
+// //         {count++;
+// //         }
+// //     }
+// //         return count;
+// // }
+// // //FOREACH FUNCTION
+ 
+// // let arr =[1,2,3,4,5];
+// // arr.forEach(function printval(val) 
+// // {
+// //     console.log(val);
+
+// // }) //foreach method is highr order function
+// // //SQUARE OF EACH NUMBER WITH FOREACH METHOD
+// let num =[1,5,6,9,3];
+//  num.forEach((num) =>{
+//     console.log(num*num);
+//  })
+//LARGEST NUMBER FIND OUT WITH REDUCE ARRAY
+// let arr=[1,2,3,4,5,6,7];
+
+// const output = arr.reduce((prev ,curr) => {
+//     return prev > curr ? prev : curr ;
+//     });
+//     console.log(output);
+//90 plus marke print with array
+// let marks= [56,70,95,99,94,45,96];
+
+// let topppers = marks.filter((val) => {
+// return val > 90;
+// })
+// console.log(topppers);
+//ASK USER NUMBER 1 TO N SUM WITH REDUCE METHOD 
+let n = prompt("Enter a number");
+let arr = [];   
+for(let i=1 ; i<=n; i++) {
+    arr [i-1]= i;
+}
+console.log(arr);
+let sum = arr.reduce((res ,curr) =>{
+    return res + curr;
+});
+console.log(sum);
