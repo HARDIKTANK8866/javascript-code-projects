02fa7ae74b02bccfab10b3ef005f39967d8b2d5a
Odan Infotech <htank8866@gmail.com>
2024-03-07 11:10:04 +0530

    update folder

--- DOM/Events.js
+++ DOM/Events.js
@@ -48,10 +48,12 @@ modeBtn.addEventListener("click", ()=>
     if (currMode ==="light")
      {
         currMode ="dark";
+        document.querySelector("body").style.backgroundColor = "black";
         document.querySelector("body")
     } else 
     {
         currMode ="light";
+        document.querySelector("body").style.backgroundColor = "white";
     }
     console.log(currMode);
 });
