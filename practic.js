cf571a2bc9c4c8772b14915903a2b95051c3a8b5
Odan Infotech <htank8866@gmail.com>
2024-03-08 16:39:48 +0530

    practie

---
+++ practic.js
@@ -0,0 +1,42 @@
+ const accountId = 12345;
+let fullName ="Hardik";
+var accountEmail ="abc@gamil.com";
+
+// //console.log(accountEmail);
+// console.table([accountId,fullName,accountEmail]);
+
+// // Prefer not to use var
+// // beacause of issue in block scope and fucntional scope
+// let name = "chiranjeev"
+// let age = 20
+// let isLoggedIn = true
+// let state
+
+// // datatypes
+// // number => 2^53
+// // bigInt
+// // string = ""
+// // boolean => true/false
+// // null (its stand-alone value)
+// // undefined =>
+// // symbol => unique
+
+// // object
+// // typeof
+
+// console.log(typeof "namaste");
+// console.log(typeof age);
+// console.log(typeof isLoggedIn);
+// console.log(typeof state);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+// // HOW TO DATATYPE FIND OUT
+  let score = 123
+//  console.log(typeof score);
+//  console.log(typeof (score));
+let str1 = "hello";
+let str2 = "hardik";
+let str3 = str1 + str2 
+console.log(str3);
