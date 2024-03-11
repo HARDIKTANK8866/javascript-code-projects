31c4d010c761b3e46afe1b6581a056d3ee657ad1
Odan Infotech <htank8866@gmail.com>
2024-03-11 14:55:21 +0530

    updated file

--- PRACTICE/practice.js
+++ PRACTICE/practice.js
@@ -60,7 +60,7 @@ const bigNumber = 12345566666666666666666n
 const heros =["SHAKTIMAN","KRISH","MASK"];
 let myObj = {
           name: "HARDIK",
-          age : 26
+         // age : 26
 } 
 
 const myFunction = function (){
@@ -113,3 +113,143 @@ const hundreds = 1000000
 // console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.sqrt(25));
+// console.log(Math.random());
+// console.log(Math.random()*10) + 1;
+// console.log(Math.floor(Math.random()* 10 ) + 1);
+
+const min = 10
+const max = 20
+// console.log(Math.floor(Math.random()* (max - min + 1) ) + min);
+
+const myDate = new Date()
+//  console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+
+//let myCreatedDate = new Date(2024, 0, 3)          //1
+// let myCreatedDate = new Date(2024, 0, 3, 5, 3)         //2
+let myCreatedDate = new Date(2024, 0, 3, 5, 3)
+
+
+// console.log(myCreatedDate.toDateString());            // 1
+// console.log(myCreatedDate.toLocaleDateString());        //2
+
+let myTimeStamp = Date.now()
+//console.log(Math.floor(Date.now()/ 1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+// newDate.toLocaleDateString('default',{
+//     weekday :"long", 
+// })
+// console.log(newDate);
+
+
+//ARRAY DECLARATION
+
+const myArr = [0,1,2,3,4,5]
+const myHeros =["shaktiman", "antman"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[1]);
+
+//ARRAY METHOD
+
+// myArr.push(6)
+// myArr.pop()
+myArr.unshift(9)
+myArr.shift()
+//console.log(myArr);
+
+//slice and spilce  diffrent
+
+//console.log(myArr);
+
+const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+// console.log(myArr);
+
+const anotherArray = [1,2,3,[4,5,6],7,8,[9,9,[1,3,3]],9,0]
+// console.log(anotherArray);
+
+const newAA =anotherArray.flat(Infinity)            //used .flat() array join karva mate
+//console.log(newAA);
+
+
+//OBJECT
+
+const mysym = Symbol("key1")
+
+const JsUser = {
+
+    name : "Hardik",
+    [mysym]: "mykey1",
+    age : 25,
+    Location : "Surat" ,
+    email : "htank8866@google.com",
+    isLoggedIn : false
+}
+JsUser.age=("23")
+//console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("hello JS");
+};
+JsUser.greetingtwo= function(){
+    console.log(`hello my name is ,${this.name}how are you`);
+};
+console.log(JsUser.greeting());
+console.log(JsUser.greetingtwo());
+// ============== Advance Object ==============
+
+ //let tinderUser = new Object(); //key difference is this is singleton object
+ 
+ const tinderUser = {};
+ tinderUser.id = "TU123";
+ tinderUser.name = "Naman";
+ tinderUser.isLoggedIn = true;
+ console.log(tinderUser);
+
+ let facebookUser = {
+     email: "someone@google.com",
+     fullName: {
+         userFullName: {
+             firstName: "hardu",
+             lastName: "tank"
+         }
+     }
+ }
+console.log(facebookUser); //complete object
+console.log(facebookUser.fullName); //nested-object
+console.log(facebookUser.fullName.userFullName.firstName); //nested-object
+console.log(facebookUser.fullName.userFullName.lastName); //nested-object
+
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {3: "a", 4: "b", 5: "c"}
+const obj4 = {1: "a", 2: "b", 3: "c"}
+
+
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2,obj4)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.values(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
