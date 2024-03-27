e55630bf35e37bad8200c9633b7362cdd4133d0f
Odan Infotech <htank8866@gmail.com>
2024-03-27 18:49:28 +0530

    new code added

---
+++ BASIC/loops.js
@@ -0,0 +1,52 @@
+                        //for loop
+// calculate sum of 1 to 5 num
+// let sum =0
+// for (let i = 1; i <= 5 ; i++) {
+//      sum = sum + i;
+//      console.log(sum);    
+// }
+
+                        //while loop
+
+// let e = 1
+// while (e<=10) {
+//     console.log("e is :",e);
+//     e++;
+// }
+                        //do-while loop
+// let e = 1
+// do {
+//     console.log("e is :",e);
+//     e++
+// } while (e<=5);
+
+                        //for of loop
+let str = "JavaScript"
+let size = 0;
+for (let i of str) {
+    // console.log("i= ",i);
+    size++;
+}
+// console.log("size is = ",size);
+
+//for-in loop
+
+let student={
+    name: "Hardik tank",
+    age : 26,
+    cgpa : 7.78,
+    isPass : true,
+}
+for (const key in student) {
+    //    console.log("key =",key,",value is =",student[key]);
+    }
+
+                                    //print all even numbers from  0 to 100 
+for (let i = 0; i <=100; i++)
+ {
+     if (i%2===0) {
+        // console.log("Even num is =",i);
+     }
+     
+}
+
