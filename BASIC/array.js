a972e45a4f3bdc087fe948797c181c5cecf25a22
Odan Infotech <htank8866@gmail.com>
2024-03-29 19:09:16 +0530

    new code added

--- BASIC/array.js
+++ BASIC/array.js
@@ -25,6 +25,39 @@ console.log(`Remove element with shift: ${fruits.shift()}`);                //Ar
 console.log(fruits);
 console.log(`Add element with unshift: ${fruits.unshift('Lemon')}`);        //Array.unshift()
 console.log(fruits);
-fruits[0] = 'Cherry'                                                        //Replace [0] to Cherry
+fruits[0] = 'Cherry'                                                    //Replace [0] to Cherry
 console.log(fruits);
-fruits[fruits.length]="Cherry"
+console.log(`The first fruits is :`,fruits[0]);
+//delete fruits[0];                                                           //Delete item
+console.log(`The first fruits is :`,fruits[0]);
+console.log("After delete array is : ",fruits);
+
+// const myGirls = ["Sarika","Taruna"]
+// const myBoys = ["Nehanshu","Sagar","Nilesh","Pratik"]
+// const myTeam = myGirls.concat(myBoys);                                       //Array.concat()
+// console.log(`Use concat array is :`, myTeam);
+
+// console.log(myTeam);
+// console.log("Copy Array",myTeam.copyWithin(2,0));
+// console.log("Another Copy Array",myTeam.copyWithin(2,0,2));
+
+// const Fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// Fruits.copyWithin(2);
+// console.log(Fruits);
+
+const num = [[1,2],[3,4],5,[6,7],8,[9,0]]
+// console.log(num);
+newNum = num.flat()                                                             //Array.flat()
+// console.log("Sub array is : ",newNum);
+
+const abc = ["a","b","c","d","e","f","g","h"]                                     //Array.slice()
+console.log("Array slice is: ",abc.slice(2,-5));
+console.log(abc);
+
+
+const A = ["a","b","c","d","e","f","g","h"]                                     //Array.splice()
+// console.log(A.splice(3,2,"A","B"));
+console.log("Array spllice is : ",A);
+
+const spliced = A.toSpliced(2)
+console.log(spliced);
