0108e4961b0aa51d28b2ef45087e56bbf8e8690a
Odan Infotech <htank8866@gmail.com>
2024-04-10 18:54:43 +0530

    new file add

--- BASIC/dateObj.js
+++ BASIC/dateObj.js
@@ -22,9 +22,26 @@ const days = ['Sunday',
 'Saturday']
 
 const currentDate =  new Date()
-console.log(currentDate);
-const Year = Date.getFullYear()
-console.log(Year);
+console.log("Currentdate :",currentDate);
 
-const times = date.getTime()
-console.log(times); 
+const date = new Date();
+
+const year = date.getFullYear()
+const month = date.getMonth()
+const day = date.getDay()
+const hours = date.getHours()
+const minutes = date.getMinutes()
+const second = date.getSeconds()
+const milisec = date.getMilliseconds()
+const dayofweek = date.getDay()
+
+
+console.log("Running year is :",year);
+console.log("Running month is :",month);
+console.log("Running day is :",day);
+console.log("Running hours is :",hours);
+console.log("Running min is :",minutes);
+console.log("Running second  is :",second);
+console.log("Running mili se is :",milisec);
+console.log("Running year is :",dayofweek);
+
