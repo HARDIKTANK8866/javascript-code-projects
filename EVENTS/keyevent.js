62c5637f898da57a3a72601a5186c1fcfcd86289
Odan Infotech <htank8866@gmail.com>
2024-04-13 18:44:45 +0530

    update more events

---
+++ EVENTS/keyevent.js
@@ -0,0 +1,10 @@
+const nameInput = document.getElementById('name')
+
+
+nameInput.addEventListener('keypress',function(){
+console.log("You press the key");
+})
+
+nameInput.addEventListener('keydown',function(){
+    console.log("You press the key");
+    })
--- EVENTS/mouse.js
+++ EVENTS/mouse.js
@@ -20,3 +20,35 @@ heading.addEventListener('mouseenter',function(){
 heading.addEventListener('mouseleave',function(){
     heading.classList.remove('red');
 })
+
+
+
+//event object
+let btn1 = document.querySelector("#btn1")
+
+btn1.onclick = (evt) => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+}
+
+// event listeners
+
+btn1.addEventListener("click",()=>{
+    console.log("button was clicked handler 1");
+})
+
+btn1.addEventListener("click",()=>{
+    console.log("button was clicked handler 2");
+})
+
+const handler3 = ()=>{
+
+    console.log("button was clicked handler 3");
+}
+
+btn1.addEventListener("click",()=>{
+    console.log("button was clicked handler 4");
+})
+
+btn1.removeEventListener("click",handler3)
