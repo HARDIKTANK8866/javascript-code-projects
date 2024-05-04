69bb7840d490e1b701be946d6ddb84a402781482
Odan Infotech <htank8866@gmail.com>
2024-05-04 19:22:31 +0530

    add new project

---
+++ PROJECTS/store/index.js
@@ -0,0 +1,8 @@
+import './src/toggleSidebar.js';
+import './src/cart/toggleCart.js';
+import './src/cart/setupCart.js';
+
+import fetchProducts from './src/fetchProducts.js';
+import { setupStore, store } from './src/store.js';
+import display from './src/displayProducts.js';
+import { getElement } from './src/utils.js';
